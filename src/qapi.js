41fc35b0e656de8470ce14a4d3897e6379019e0b
pratipkumaraa <tpratip@gmail.com>
2020-11-14 18:21:03 +0530

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fafcacc2c4ef2001e325004",
+				"5fafcacc2c4ef2001e325006"
+			]
+		},
+		"5fafcacc2c4ef2001e325004": {
+			"id": "5fafcacc2c4ef2001e325004",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fafcacc2c4ef2001e325006": {
+			"id": "5fafcacc2c4ef2001e325006",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
